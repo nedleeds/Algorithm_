218ca0ae75daedffa61b9ed84375052c57038165
nedleeds <lllee.dh@gmail.com>
2022-09-06 21:23:53 +0900

    Fix : SWEA A dfs boj17070(G5)

--- Problems/SWEA/02_swea_A_boj17070.cpp
+++ Problems/SWEA/02_swea_A_boj17070.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 // G5
-int n, ans = -2134567890;
 string str;
+int MAP[18][18];
+int visited[18][18];
+int n, ans;
 
-int calculate(int a, int b, char op) {
-	if (op == '+')
-		a += b;
-	else if (op == '-')
-		a -= b;
-	else if (op == '*')
-		a *= b;
+struct Pos { int r, c; } start = { 1, 2 };
 
-	return a;
-}
+int dir[3][2] = {
+	{0, 1},
+	{1, 0},
+	{1, 1}
+};
 
-void dfs(int idx, int cur) {
-	if (idx >= n) {
-		ans = max(ans, cur);
+void dfs(Pos now, int mode) {
+	if (now.r == n && now.c == n) {
+		ans++;
 		return;
 	}
 
-	char op = idx == 0 ? '+' : str[idx - 1];
+	// 0:가로, 1:세로, 2:대각선
+	for (int nextMode = 0; nextMode < 3; nextMode++) {
+		if (mode == 0 && nextMode == 1) // 현재 가로모드, 다음 세로모드
+			continue;
+		if (mode == 1 && nextMode == 0) // 현재 세로모드, 다음 가로모드
+			continue;
+		Pos next = { now.r + dir[nextMode][0], now.c + dir[nextMode][1] };
+		int nr = next.r;
+		int nc = next.c;
+		if (nr <= 0 || nc <= 0 || nr > n || nc > n)
+			continue;
+		if (visited[nr][nc] != 0)
+			continue;
+		if (MAP[nr][nc] == 1)
+			continue;
+		if (nextMode == 2) {  // 다음 대각모드 - 벽 체크
+			if (MAP[nr - 1][nc] || MAP[nr][nc - 1])
+				continue;
+		}
 
-	if (idx + 2 < n) {
-		int bracket = calculate(str[idx] - '0', str[idx + 2] - '0', str[idx + 1]);
-		dfs(idx + 4, calculate(cur, bracket, op));
+		visited[nr][nc] = 1;
+		dfs(next, nextMode);
+		visited[nr][nc] = 0;
 	}
-
-	dfs(idx + 2, calculate(cur, str[idx] - '0', op));
-
 }
 
 int main() {
@@ -38,15 +51,16 @@ int main() {
 	cin.tie();
 	cout.tie();
 
-	cin >> n >> str;
+	cin >> n;
 
-	dfs(0, 0);
+	for (int r = 1; r <= n; r++)
+		for (int c = 1; c <= n; c++)
+			cin >> MAP[r][c];
+
+	visited[1][2] = 1;
+	dfs({ 1, 2 }, 0);
 
 	cout << ans;
 
 	return 0;
 }
-/*
-
---->
-*/
