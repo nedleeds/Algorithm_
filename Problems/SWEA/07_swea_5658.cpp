d151b4d2fd84915acdbe7ff2c21a99759f1df687
will <lllee.dh@gmail.com>
2022-10-06 20:59:49 +0900

    Fix : swea 5658 (works in VS)

--- Problems/SWEA/07_swea_5658.cpp
+++ Problems/SWEA/07_swea_5658.cpp
@@ -1,62 +1,79 @@
 #include <iostream>
 #include <vector>
-#define STEPSIZE 3
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
 int testCase, totalLength, K;
+vector<int> duplicates;
 string numbers;
 
-string getHexa(int start, int totalLength){
-    string currHexa = "";
-    for (int step=0; step<STEPSIZE; step++){
-        int idx = (start+step)%totalLength;
-        currHexa += numbers[idx];
-    }
-    return currHexa;
+char* getHexa(int start, int totalLength) {
+	// the lib is 'square' -> totalLength/4
+	string currHexa;
+	for (int step = 0; step < (totalLength / 4); step++) {
+		int idx = (start + step) % totalLength;
+		currHexa += numbers[idx];
+	}
+	
+	char hex[10001];
+	strcpy(hex, currHexa.c_str());
+	return hex;
 }
 
-bool cmp(int a, int b){
-    if (a < b) return true;
-    if (a > b) return false;
-    else return false;
+bool isDuplicate(int decValue) {
+	for (int i = 0; i < duplicates.size(); i++) {
+		int curr = duplicates[i];
+		if (curr == decValue)
+			return 1;
+	}
+	duplicates.push_back(decValue);
+	return 0;
+}
+
+bool cmp(int a, int b) {
+	if (a > b) return true;
+	if (a < b) return false;
+	else return false;
 }
 
 int main()
 {
-    // char* a = "0xFF";
-    // cout << "Hex: " << a << '\n';
-    // cout << "Dec: " << (int)strtol(a, NULL, 16) << '\n';
-    ios_base::sync_with_stdio(false);
-    cin.tie();
-    cout.tie();
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
 
-    cin >> testCase;
-    for (int tc=1; tc<=testCase; tc++){
-        // get the numbers
-        cin >> totalLength >> K >> numbers;
+	cin >> testCase;
+	for (int tc = 1; tc <= testCase; tc++) {
+		// get the numbers
+		cin >> totalLength >> K >> numbers;
+		duplicates.clear();
 
-        // set the indices for numbers with given STEPSIZE
-        // set the hexValues
-        vector<int>  hexValues;
-        for (int i=0; i<totalLength; i++){
-            // get the step indices
-            string currHexString = getHexa(i, totalLength);
-            int currHex = stoul(currHexString, NULL, 16);
-             hexValues.push_back(currHex);
-            // cout << "currHex: "<< currHexa << "\n";
-        }   
-        sort(hexValues.begin(), hexValues.end(), cmp);
-        // cout << hexValues[K] << '\n';
-        for (int i=0; i<totalLength; i++){
-            cout << hexValues[i] << '\n';
-        }
-        printf("%d\n", hexValues[K]);
-    }
+		vector<int>  decValues;
+		for (int i = 0; i < totalLength; i++) {
+			// check 1 edge
+			int decValue = (int)strtol(getHexa(i, totalLength), NULL, 16);
 
-    return 0;    
+			if (!isDuplicate(decValue)) {
+				decValues.push_back(decValue);
+			}
+		}
+		sort(decValues.begin(), decValues.end(), cmp);
+		cout << "# " << decValues[K - 1] << '\n';
+	}
+
+	return 0;
 }
 /*
-1
+5
 12 10
 1B3B3B81F75E
+16 2
+F53586D76286B2D8
+20 14
+88F611AE414A751A767B
+24 16
+044D3EBA6A647B2567A91D0E
+28 11
+8E0B7DD258D4122317E3ADBFEA99
 */
