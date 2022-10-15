70256fb00b557bb691fc7e318a394c9b0cfa86b4
will <lllee.dh@gmail.com>
2022-10-15 18:49:03 +0900

    BOJ : 1874 S2 (point -> hard to understand problem)

--- Problems/BOJ/07_boj_1874.cpp
+++ Problems/BOJ/07_boj_1874.cpp
@@ -1,65 +1,48 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int total;
-int ansSheet[1000001];
-int movedArr[1000001];
-string path;
-int popCnt;
-int pushCnt;
-int flag;
-
-int doOperate() {
-	int arr[] = { 1,2,3,4,5,6,7,8 };
-	while (!path.empty()) {
-
-	}
-
-}
-
-void comb(int lvl) {
-	if (lvl == 2*total) {
-		if (pushCnt <= popCnt) {
-			flag = 0;
-			return;
-		}
-		else {
-			if (doOperate()){
-				flag = 1;
-				return;
-			}
-			else flag = 0;
-		}
-		return;
-	}
-
-
-	for (int branch = 0; branch < 2; branch++) {
-		if (branch == 0) popCnt++;
-		else pushCnt++;
-		path += ('0' + branch);
-		
-		comb(lvl + 1);
-
-		if (branch == 0) popCnt--;
-		else pushCnt--;
-		path.pop_back();
-	}
-
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 
-	cin >> total;
-	for (int i = 0; i < total; i++) {
-		cin >> ansSheet[i];
-		movedArr[i] = i + 1;
+	freopen_s(new FILE*, "sample_input.txt", "r", stdin);
+
+	int N;
+	cin >> N;
+
+	vector<int> stack, result;
+	int cnt = 1;
+	int isPass = true;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+
+		// push should be increasing order
+		while (cnt <= num) {
+			stack.push_back(cnt++);
+			result.push_back('+');
+		}
+
+		if (stack[stack.size() - 1] == num) {
+			stack.pop_back();
+			result.push_back('-');
+		}
+		else {
+			isPass = false;
+			break;
+		}
 	}
 
-	comb(0);
+	if (isPass) {
+		for (int i = 0; i < result.size(); i++)
+			cout << (char) result[i] << '\n';
+	}
+	else {
+		cout << "NO\n";
+	}
 
 	return 0;
 }
