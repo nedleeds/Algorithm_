04daad51ed0b5e41a72e906c9ab578ba8c2d0205
will <lllee.dh@gmail.com>
2022-10-20 23:18:08 +0900

    Add : BOJ 2805 (point -> dataType, binarySearch)

--- Problems/BOJ/09_boj_2805.cpp
+++ Problems/BOJ/09_boj_2805.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#define INF 2134567890
+#define INT long long
 using namespace std;
+
+INT N, M, H, ANS;
+vector<INT> trees;
+
+INT sum_diff(INT mid) {
+	INT tot = 0;
+	for (INT i = 0; i < N; i++) {
+		if (trees[i] > mid)
+			tot += trees[i] - mid;
+	}
+	return tot;
+}
+
+void binarySearch(INT left, INT right) {
+	// left, right, mid -> "tree height"
+
+	while (left <= right) {
+		INT mid = (left + right) / 2;
+		if (sum_diff(mid) >= M) {
+			// renew left
+			if (ANS < mid) {
+				ANS = mid;
+			}
+			left = mid + 1;
+		}
+		else{
+			// renew right
+			right = mid - 1;
+		}
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, m, h;
-	cin >> n >> m;
+	freopen_s(new FILE*, "sample_input.txt", "r", stdin);
 
-	vector<int> trees;
-	while (n--) {
-		cin >> h;
-		trees.push_back(h);
-	}
+	cin >> N >> M;
+	// 0 < N <= 10^6
+	// 0 < M <= 2 * 10^9 
+	// Type should be changed from 'int' to 'long long'
 	
+	INT hMax = -INF;
+	INT n = N;
+	while (n--) {
+		cin >> H;
+		trees.push_back(H);
+		hMax = max(hMax, H);
+	}
 
+	binarySearch(0, hMax);
+
+	cout << ANS << '\n';
 
 	return 0;
 }
