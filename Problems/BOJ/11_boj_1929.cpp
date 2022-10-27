e09beaec41152adac9b1a2ce6bc630e8cf3dec41
nedleeds <lllee.dh@gmail.com>
2022-10-27 09:58:22 +0900

    Add : BOJ 1929 (point -> Eratos)

---
+++ Problems/BOJ/11_boj_1929.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+bool* PrimeArray = new bool[1000001];
+
+void Eratos(int n) {
+	if (n <= 1)
+		return ;
+
+	// init
+	for (int i = 2; i <= n; i++)
+		PrimeArray[i] = true;
+	
+	for (int i = 2; i * i <= n; i++) {
+		if (PrimeArray[i])
+			for (int j = i * i; j <= n; j += i)
+				PrimeArray[j] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	freopen_s(new FILE*, "sample_input.txt", "r", stdin);
+
+	int M, N;
+	cin >> M >> N;
+
+	Eratos(N);
+
+	for (int k = M; k <= N; k++) {
+		if (PrimeArray[k]) {
+			cout << k << '\n';
+		}
+	}
+
+
+	return 0;
+}
