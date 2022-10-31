1b569f95816f935259be4e718578ca3e22174cc9
nedleeds <lllee.dh@gmail.com>
2022-10-31 17:13:42 +0900

    Add : BOJ 1107

---
+++ Problems/BOJ/17_boj_1107.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	freopen_s(new FILE*, "sample_input.txt", "r", stdin);
+
+	int target;
+	cin >> target;
+
+	int errorCnt;
+	cin >> errorCnt;
+	vector<int> errors;
+	for (int i = 0; i < errorCnt; i++) {
+		int error;
+		errors.push_back(error);
+	}
+
+	return 0;
+}
