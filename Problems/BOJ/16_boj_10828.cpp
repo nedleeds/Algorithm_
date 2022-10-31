909dba7db6fa9fa5f9dd1f667b3a4187545f0c53
nedleeds <lllee.dh@gmail.com>
2022-10-31 13:03:15 +0900

    Add : BOJ 10828 (stack-push/pop)

---
+++ Problems/BOJ/16_boj_10828.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void operate(string cmd, void* stackP, int num){
+	vector<int> stack = *(vector<int> *) stackP;
+
+	if (cmd == "push"){
+		stack.push_back(num);
+		int de = 1;
+	}
+	else if (cmd == "pop"){
+		int num = stack.empty()? -1 : stack[stack.size()-1];
+		stack.pop_back();
+		cout << num << '\n';
+		int de = 1;
+	}
+	else if (cmd == "size"){
+		cout << stack.size() << '\n';
+		int de = 1;
+	}
+	else if (cmd =="empty"){
+		int isEmpty = stack.empty() ? 1 : 0;
+		cout << isEmpty << '\n';
+		int de = 1;
+	}
+	else if (cmd == "top"){
+		int num = stack.empty()? -1 : stack[stack.size()-1];
+		cout << num << '\n';
+		int de = 1;
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	freopen("../SampleInput/input.txt", "r", stdin);
+
+	int n;
+	cin >> n;
+	
+	vector<int> stack;
+	for (int i = 0; i < n; i++){
+		string s;
+		getline(cin, s);
+
+		int idx = s.find(' ');
+		string cmd = s.substr(0, idx);
+
+		int num = 0;
+		if (idx != - 1)
+			num = atoi(s.substr(idx + 1, s.size()-1).c_str());
+
+		operate(cmd, &stack, num);
+	}
+
+	return 0;
+}
