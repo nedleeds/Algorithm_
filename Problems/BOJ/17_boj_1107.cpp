ba782bf5a0a4993a70e3dd721dc9667aa39d56b2
dhl <lllee.dh@gmail.com>
2022-11-03 21:50:11 +0900

    Fix : BOJ 1107(point -> BruteForce, case2(+/-, press numbers))

--- Problems/BOJ/17_boj_1107.cpp
+++ Problems/BOJ/17_boj_1107.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
 using namespace std;
 
-typedef struct inputs{
-	string num;
-	int cnt;
-	int brokenNum;
-	bool broken[11] = {false, };
-}Input;
+int target;
+int errCnt, btn;
+bool errors[10] = {false};
 
-void setInputs(Input *arg){
-	cin >> arg->num;
-	arg->cnt = arg->num.size();
-	cin >> arg->brokenNum;
-
-	for (int i = 0; i < arg->brokenNum; i++){
-		int idx;
-		cin >> idx;
-		arg->broken[idx] = true;
-	}
+int countPress(int num){
+    int cnt = 0;
+    // 0 -> hanling
+    if(num == 0){
+        if (errors[0]) 
+            return -1;
+        return 1;
+    }
+    // 0 < num, press the button
+    while(num){
+        if (errors[num % 10]) // is error
+            return -1;
+        num /= 10;
+        cnt++; // count: length of num
+    }
+    
+    return cnt;
 }
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie();
-	cout.tie();
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
 
-	freopen("../SampleInput/input.txt", "r", stdin);
+    freopen("../SampleInput/input.txt", "r", stdin);
 
-	Input arg;
-	setInputs(&arg);
+    // target
+    cin >> target;
 
+    // check error button
+    cin >> errCnt;
+    for (int i = 0; i < errCnt; i++){
+        cin >> btn;
+        errors[btn] = true;
+    }
 
-	// check whether broken or not
-	for (int i=0; i<arg.num.size(); i++){
+    // check start(100) and target is same
+    if (target == 100){
+        cout << "0\n";
+        return 0;
+    }
 
-	}
-
-	return 0;
+    int ans = abs(target - 100);
+    for (int i = 0; i <= 999999; i++){
+        int cnt = countPress(i);
+        if (cnt > 0){
+            // if no error button
+            ans = min(ans, cnt + abs(target - i));
+        }
+    }
+    cout << ans << '\n';
+    return 0;
 }
