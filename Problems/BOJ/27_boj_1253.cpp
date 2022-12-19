a871373ff8fef36a5aa642076ee88c5892a0a2e5
dhl <lllee.dh@gmail.com>
2022-12-19 18:29:35 +0900

    Add : BOJ 1253 (point -> two pointer)

--- Problems/BOJ/27_boj_1253.cpp
+++ Problems/BOJ/27_boj_1253.cpp
@@ -49,3 +49,15 @@ int main(){
     Values.clear();
     return 0;
 }
+/*
+첫 시도: brute force 로 접근
+       - 2중 for 문을 돌면서 서로 다른 index 2개를 합쳐서 target 과 비교
+       - N: 2000 이라, 괜찮다고 생각할 수 있지만, 주어진 수에 대해 2중 for 문
+         -> 최종적으로 3 중 for 문을 돌게 된다
+         -> O(N ** 3) -> 시간 초과
+
+두번째 시도: two_pointer
+       - 현재 value 를 제외한 value 를 새로운 vector 에 저장
+       - 그리고, sort
+       - left, right 를 활용해 부분 합 비교
+*/
