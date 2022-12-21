f23b3b72159eafb3440cae48bc533165dcfee71a
dhl <lllee.dh@gmail.com>
2022-12-21 23:52:56 +0900

    Fix : BOJ 16937 (point -> Brute force, using for, case check)

--- Problems/BOJ/29_boj_16937_lc.cpp
+++ Problems/BOJ/29_boj_16937_lc.cpp
@@ -23,18 +23,19 @@ int main(){
 
     int ans = 0;
     for (int i = 0; i < n; i++){
-        int r1 = Stickers[i].r, c1 = Stickers[i].c;
-        for (int j = 0; j < n; j++){
-            if (i == j) continue;
-            int r2 = Stickers[j].r, c2 = Stickers[j].c;
+        int r1 = Stickers[i].r;
+        int c1 = Stickers[i].c;
+        for (int j = 0; j < n && i != j; j++){
+            int r2 = Stickers[j].r; 
+            int c2 = Stickers[j].c;
             for (int rot1 = 0; rot1 < 2; rot1++){
                 for (int rot2 = 0; rot2 < 2; rot2++){
                     // 아래쪽으로 붙이기: r1 + r2
-                    if (r1 + r2 < h && max(c1, c2) < w){
+                    if (r1 + r2 <= h && max(c1, c2) <= w){
                         ans = max(ans, r1 * c1 + r2 * c2);
                     }
                     // 옆으로 붙이기: c1 + c2
-                    if (max(r1, r2) < h && c1 + c2 < w){
+                    if (max(r1, r2) <= h && c1 + c2 <= w){
                         ans = max(ans, r1 * c1 + r2 * c2);
                     }
                     swap(r2, c2);
