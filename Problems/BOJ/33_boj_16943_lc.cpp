7361cc4d30657a9055f4418bacc6e859b3b60f08
dhl <lllee.dh@gmail.com>
2022-12-29 13:07:06 +0900

    Fix : BOJ 16943_lc(point -> bruteForcing for all permutation)

--- Problems/BOJ/33_boj_16943_lc.cpp
+++ Problems/BOJ/33_boj_16943_lc.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int DAT[1000000000];
+string A, B, C;
+int Ans;
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -9,37 +11,28 @@ int main(){
 
     freopen("../SampleInput/input.txt", "r", stdin);
 
-    string a, b, c;
-    int ans = -1;
+    cin >> A >> B;
+    Ans = -1;
 
-    cin >> a >> b;
-
-    bool dat[10] = {false,};
-    for (int i = 0; i < a.size(); i++){
-        dat[i] = true;
-        for ( int j = 0; j < a.size(); j++){
-            if (dat[j]) { continue; }
-            dat[j] = true;
-            for (int k = 0; k < a.size(); k++){
-                if (dat[k]) { continue; }
-                dat[k] = true;
-                for (int l = 0; l < a.size(); l++){
-                    if (dat[l]) { continue; }
-                    cout << a[i] << a[j] << a[k] << a[l] << '\n';
-                    // int num = 
-                    // DAT[]
-                }
-                dat[k] = false;
-            }
-            dat[j] = false;
+    sort(A.begin(), A.end());
+    do{
+        if (A[0] != '0' and stoi(A) < stoi(B) and Ans < stoi(A)){
+            Ans = stoi(A);
         }
-        dat[i] = false;
-    }
+    }while(next_permutation(A.begin(), A.end()));
+
+    cout << Ans << '\n';
 
     return 0;
 }
+
 /*
 1차 시도 : for 문으로 구현하려고 했지만,
          구현하다보니, A가 몇 자리의 정수라는 말이 없음
          4중 for 로 구현 못 할 가능성이 있음 -> dfs
+
+2차 시도 : dfs 에서 test case 에러가 뜸
+3차 시도 : 해설을 보았고, do-while + next_permutation 으로 맞춤
+          -> 모든 순열은 sort + do-while + next_permutation 좋음
+4차 풀이 : 
 */
