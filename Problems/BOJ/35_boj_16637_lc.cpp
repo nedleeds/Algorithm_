aaaa9fde808f4b6b7bf32fe7931041bce396fd67
dhl <lllee.dh@gmail.com>
2023-01-04 06:51:57 +0900

    Fix : BOJ 16637_lc(point -> BruteForce with Bitmask)

--- Problems/BOJ/35_boj_16637_lc.cpp
+++ Problems/BOJ/35_boj_16637_lc.cpp
@@ -10,7 +10,7 @@ int main(){
 
     freopen("../SampleInput/input.txt", "r", stdin);
 
-    int n;
+    int n, maxAns;
     string s;
     cin >> n >> s;
 
@@ -26,6 +26,7 @@ int main(){
 
     // m : 연산자 수
     // 연산자 수를 기준으로 비트마스크 진행
+    maxAns = 0;
     int m = (n - 1) / 2;
     for (int i = 0; i < (1 << m); i++){
         bool ok = true;
@@ -33,16 +34,44 @@ int main(){
             // 연속된 연산자 2개를 선택하는 경우 -> 제외
             if ((i & (1 << j)) && (i & (1 << (j + 1)))){ ok = false; }
         }
-
-        for (int j = 0; j < m)
-        
-        // 선택된 연산자는 먼저 계산 진행
-        int compute = 0;
-            
-
+        // 연산자 기준으로 계산
+        if (!ok){ continue; }
+        vector<Var> temp(variables); 
+        for (int j = 0; j < m; j++){
+            if ((i & (1 << j))){
+                int k = 2 * j + 1;
+                if (variables[k].op == '+'){
+                    temp[k - 1].num += temp[k + 1].num;
+                }
+                else if(variables[k].op == '-'){
+                    temp[k - 1].num -= temp[k + 1].num;
+                }
+                else if(variables[k].op == '*'){
+                    temp[k - 1].num *= temp[k + 1].num;
+                }
+                temp[k].op = '+';
+                temp[k+1].num = 0;
+            }
         }
+        
+        int res = temp[0].num;
+        for (int l = 0; l < m; l++){
+            int k = 2 * l + 1;
+            if (temp[k].op == '+'){
+                res += temp[k + 1].num;
+            }
+            else if (temp[k].op == '-'){
+                res -= temp[k + 1].num;
+            }
+            else if (temp[k].op == '*'){
+                res *= temp[k + 1].num;
+            }
+        }
+
+        if (maxAns < res){ maxAns = res; }
     }
 
+    cout << maxAns << '\n';
 
     return 0;
 }
