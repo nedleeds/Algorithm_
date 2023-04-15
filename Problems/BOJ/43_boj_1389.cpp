5e0bf6506cfd3fff7fe5f84baf9a661efc8f5ae3
dhl <lllee.dh@gmail.com>
2023-04-15 22:31:26 +0900

    Fix : BOJ 1389 - dijkstra

--- Problems/BOJ/43_boj_1389.cpp
+++ Problems/BOJ/43_boj_1389.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
 #include <vector>
 #define INF 2134567890
 using namespace std;
 
 int N, M; // N: # of Users, M: # of friends 
+int Vst[101];
 vector<vector<int>> AdjList, Dist;
 
-void bfs(int src, int dst){
-    // linked list 의 모든 요소를 탐색하도록 해야한다
+void bfs(int start){
+    // dijkstra 로 체크
     queue<int> q;
-    q.push(src);
+    q.push(start);
 
     while(!q.empty()){
+        int user = q.front();
+        q.pop();
+
+        for (int i = 0; i < AdjList[user].size(); i++){
+            int pal = AdjList[user][i];
+            Dist[user][pal] = min(Dist[user][i], Dist[user][pal]);
+        }
 
     }
     
@@ -40,12 +49,7 @@ int main(){
     {   
         if (AdjList[user].size() != 0)
         {   // [user -> pal]'s min dist
-            for (int pal = 0; pal < N + 1; pal++)
-            {   // check each pal
-                if (pal != user){
-                    bfs(user, pal);
-                }
-            }
+            bfs(user);
         }
     }
 
